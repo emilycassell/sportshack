d196181addf70e949c9f58a2dc8408e348ae1d23
david.berman <david.berman@omnigon.com>
2018-07-25 13:15:14 -0400

    Added some bootstrap features

--- public/controllers/home/app.js
+++ public/controllers/home/app.js
@@ -1,3 +1,12 @@
+// This function will change the state of the UI item
+function updateSeat(seat, isStanding) {
+    console.log("Seat " + seat + " = " + isStanding);
+    var selector = "#seat_" + seat.toString();
+    $(selector).toggleClass("fa-user", true);
+    $(selector).toggleClass("fas", isStanding);
+    $(selector).toggleClass("far", !isStanding);
+}
+
 // Open the firebase when the page is ready
 $(document).ready(function() {
     try {
@@ -11,14 +20,21 @@ $(document).ready(function() {
             messagingSenderId: "306433267649"
         };
         firebase.initializeApp(config);
+        console.info("Firebase is initialized.");
 
         // Get a reference to the database service
         let database = firebase.database();
 
         // Subscribe to be notified of changes to the database
-        var starCountRef = firebase.database().ref('seats');
-        starCountRef.on('value', function(snapshot) {
-            alert("State of seats has changed.");
+        var seats = firebase.database().ref('seats');
+        seats.on('value', function(snapshot) {
+            console.log("State of seats has changed.");
+
+            snapshot.forEach(function(childSnapshot) {
+                var childKey = childSnapshot.key;
+                var childData = childSnapshot.val();
+                updateSeat(parseInt(childKey), childData == '1');
+            });
         });
 
 
